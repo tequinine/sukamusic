53d423006b8cb3f44d298bd6e54589aa6abe342e
Psycho <gabrieltasso22@gmail.com>
2022-05-11 14:41:38 -0300

    starmusic

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -27,7 +27,7 @@ module.exports = {
         embed.setDescription(`Volume **${queue.volume}**%\nDuration **${trackDuration}**\nLoop mode **${methods[queue.repeatMode]}**\nRequested by ${track.requestedBy}`);
 
         embed.setTimestamp();
-        embed.setFooter('Music comes first - Made with heart by Zerio ❤️', message.author.avatarURL({ dynamic: true }));
+        embed.setFooter('Music comes first - Made with heart by Psycho ❤️', message.author.avatarURL({ dynamic: true }));
 
         const saveButton = new MessageButton();
 
--- commands/music/queue.js
+++ commands/music/queue.js
@@ -28,7 +28,7 @@ module.exports = {
         embed.setDescription(`Current ${queue.current.title}\n\n${tracks.slice(0, 5).join('\n')}\n\n${nextSongs}`);
 
         embed.setTimestamp();
-        embed.setFooter('Music comes first - Made with heart by Zerio ❤️', message.author.avatarURL({ dynamic: true }));
+        embed.setFooter('Music comes first - Made with heart by Psycho ❤️', message.author.avatarURL({ dynamic: true }));
 
         message.channel.send({ embeds: [embed] });
     },
